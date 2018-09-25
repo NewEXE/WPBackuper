3ed775d4aa6ce75a6c4ebd71b1d74778b15be204
NewEXE <v.voloshyn96@gmail.com>
2018-09-26 00:45:44 +0300

    Added php version to status page; Added localization to statuses

--- admin/partials/tab-status.php
+++ admin/partials/tab-status.php
@@ -11,8 +11,10 @@
 	<?php $i = 0; ?>
     <?php foreach ($items as $item): ?>
         <tr <?php echo $i % 2 ? 'class="alternate"': '' ?>>
-            <td class="row-title"><?php echo $item['name'] ?></td>
-            <td><?php echo $item['true'] ? $item['description_true'] : $item['description_false'] ?></td>
+            <td class="row-title"><?php echo $item['name'] ?> <p class="description" style="font-weight: 400;"><?php echo $item['hint'] ?></p></td>
+            <td><?php echo $item['true'] ?
+		            '<mark class="yes"></mark> <span class="dashicons dashicons-yes"></span> ' . $item['description_true'] :
+		            '<mark class="warning"></mark> <span class="dashicons dashicons-warning"></span> ' . $item['description_false'] ?></td>
         </tr>
         <?php $i++; ?>
     <?php endforeach; ?>
